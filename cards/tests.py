6ac44728075da62c461100d650d5c4552a96516d
R-Grigala <r.grigalashvili777@gmail.com>
2024-06-03 18:06:13 +0400

    Add tests  -_-

--- cards/tests.py
+++ cards/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class CardAPITestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_user', password='password123')
+        self.client.force_authenticate(user=self.user)
+
+    def test_valid_card_creation(self):
+        data = {
+            'title': 'Test Card',
+            'card_number': '1234567890123456',
+            'ccv': '123'
+        }
+        response = self.client.post('/your-api-endpoint/', data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_invalid_card_creation(self):
+        # Test with invalid card number
+        data = {
+            'title': 'Test Card',
+            'card_number': 'invalid_card_number',
+            'ccv': '123'
+        }
+        response = self.client.post('/your-api-endpoint/', data)
+        self.assertEqual(response.status_code, 400)
+
+        # Test with invalid CCV
+        data = {
+            'title': 'Test Card',
+            'card_number': '1234567890123456',
+            'ccv': 'invalid_ccv'
+        }
+        response = self.client.post('/your-api-endpoint/', data)
+        self.assertEqual(response.status_code, 400)
